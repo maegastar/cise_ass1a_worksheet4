60d25305d7a5e377f5e06ab70962462523420b0b
Maega <maegastar@gmail.com>
2022-08-19 12:26:15 +1200

    changed UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
+import {
+  Route,
+  Routes,
+  NavLink,
+  BrowserRouter as Router,
+  Navigate
+} from "react-router-dom";
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
+
 const App = () =>  {
     return (
-        <Router>
+      
+      <Router>
         <div>
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+              <li><NavLink end to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
+            
           <div className="content">
-            <
-          </div>
+          
+        <Routes>
+          <Route  path="/" element={Home}/>
+            <Route  path="/SEPractice" element={SEPractice}/>
+            <Route  path="/SubmitArticle" element={SubmitArticle}/>
+            <Route  path="/404" element={NotFoundPage}/>
+            
+        </Routes>
+        
+        
+        </div>
         </div>
         </Router>
+        
     );
 }
  
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./App.js";
- 
-ReactDOM.render(
-  <App/>, 
-  document.getElementById("root")
-);
+import ReactDOM from "react-dom/client";
+import {default as App} from "./App.js";
+import "./index.css";
+
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <App/>
+  
+)
